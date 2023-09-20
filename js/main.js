cccf3144d49a4ac6e6ce7df1ecff0dfa3aeec1d6
Jenna Martin <jennamar828@gmail.com>
2023-09-20 18:22:21 +0000

    js pseudocode started

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* 
+Start 
+    "i can read your mind front screen"
+    go button
+switch screen (without changing the page)
+    Pick a number screen 
+        next button
+        reset button
+        "when you have number click next"
+    Add digits to get new number
+        next button
+        reset button 
+        example below next button
+    Subtract new number from original
+        next button
+        reset button
+        example and directions
+    find new number and corresponding symbol
+        symbols page
+        reveal button
+        reset button
+    symbol page
+        reset button
+End
+*/
