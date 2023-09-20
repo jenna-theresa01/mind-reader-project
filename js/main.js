fc0ddc5adbfdea9f9701f03de91f7d8d170635c5
Jenna Martin <jennamar828@gmail.com>
2023-09-20 18:32:06 +0000

    divs created

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@ Start
     "i can read your mind front screen"
     go button
 switch screen (without changing the page)
-    Pick a number screen 
+    "Pick a number screen"
         next button
         reset button
         "when you have number click next"
-    Add digits to get new number
+    "Add digits to get new number"
         next button
         reset button 
         example below next button
-    Subtract new number from original
+    "Subtract new number from original"
         next button
         reset button
         example and directions
-    find new number and corresponding symbol
+    "find new number and corresponding symbol"
         symbols page
         reveal button
         reset button
-    symbol page
+    symbol page (mind read)
         reset button
 End
 */
