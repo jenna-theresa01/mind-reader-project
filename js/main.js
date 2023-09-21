337d97d86d9b7cedfbba934f1edcf63cb29444ca
Jenna Martin <jennamar828@gmail.com>
2023-09-21 17:39:36 +0000

    button formatting

--- js/main.js
+++ js/main.js
@@ -41,13 +41,27 @@ End
 window.onload = (event) => {
     console.log("page is loaded");
 } 
+
+// this is adding a click event to my "Go" buttton
 let go_btn = document.getElementById("go")
 go_btn.addEventListener("click", myFunction);
 function myFunction() {
-
-
-
-
-    
     console.log("myFunction");
+}
+
+// this is adding a click event to my *first* "Next" buttton
+// need to figure out how to include the other two "Next" buttons
+let next_btn = document.getElementById("next")
+next_btn.addEventListener("click", nextButton);
+function nextButton() {
+    
+    console.log("nextButton");
+}
+
+// this is adding a click event to my "Go" buttton
+let reveal_btn = document.getElementById("reveal")
+reveal_btn.addEventListener("click", revealButton);
+function revealButton() {
+
+    console.log("revealButton");
 }
